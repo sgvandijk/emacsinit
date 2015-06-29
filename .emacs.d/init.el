5debd77f065c696473a70bf779d9152de2457eeb
Sander van Dijk <sgvandijk@gmail.com>
2015-06-29 20:05:47 +0100

    cleanup

--- .emacs.d/init.el
+++ .emacs.d/init.el
@@ -255,12 +255,4 @@ M-x compile.
      )
   )
 
-;(elpy-enable)
 
-;(require 'ess-site)
-(custom-set-faces
- ;; custom-set-faces was added by Custom.
- ;; If you edit it by hand, you could mess it up, so be careful.
- ;; Your init file should contain only one such instance.
- ;; If there is more than one, they won't work right.
- )
