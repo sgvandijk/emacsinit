cd88a49ebe406ae41afabe91ba67fc3e64d4bdcf
Sander van Dijk <sgvandijk@gmail.com>
2016-09-09 09:32:35 +0100

    Bring back menu bar

--- .emacs.d/init.el
+++ .emacs.d/init.el
@@ -1,7 +1,5 @@
 ;;; Begin initialization
-;; Turn off mouse interface early in startup to avoid momentary display
 (when window-system
-  (menu-bar-mode -1)
   (tool-bar-mode -1)
   (scroll-bar-mode -1)
   (tooltip-mode -1))
