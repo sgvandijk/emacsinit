3fe265fc5343f02b11c0e1e697ce857c5ddfadfa
Sander van Dijk <sander@parkopedia.com>
2018-02-13 10:33:43 +0000

    Some font and Linux changes

--- .emacs.d/local-inits/linux.el
+++ .emacs.d/local-inits/linux.el
@@ -2,12 +2,7 @@
 ;;; Code:
 ;;; Commentary:
 
-(custom-set-faces
- ;; custom-set-faces was added by Custom.
- ;; If you edit it by hand, you could mess it up, so be careful.
- ;; Your init file should contain only one such instance.
- ;; If there is more than one, they won't work right.
- '(default ((t (:inherit nil :stipple nil :inverse-video nil :box nil :strike-through nil :overline nil :underline nil :slant normal :weight normal :height 83 :width normal :foundry "unknown" :family "Liberation Mono")))))
+(require 'cl)
 
 (provide 'local)
 ;;; linux.el ends here
